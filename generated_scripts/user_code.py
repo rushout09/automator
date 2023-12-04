df9dcac69688cfdf3dfadac9870b1c20e7b13a7e
Rushabh <rushabh.agarwal9@gmail.com>
2023-12-04 10:11:39 +0530

    Added error handling, function calling and stuff

--- generated_scripts/user_code.py
+++ generated_scripts/user_code.py
@@ -1,21 +1,47 @@
 from playwright.sync_api import sync_playwright
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
-    page = browser.new_page()
-    page.goto('https://hevodata.com')
+def run(playwright):
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    # Open new page
+    page = context.new_page()
 
+    # Go to hevodata.com
+    page.goto("https://hevodata.com")
+    print("Navigated to hevodata.com")
+
+    # Find the email input box and enter a dummy email
+    print("Finding the email input box")
     try:
-        # Assuming 'Login' is a text that can be used to locate the button
-        login_button = page.wait_for_selector("text=Login")
-
-        if login_button:
-            login_button.click()
-            print('Clicked on the Login button.')
-        else:
-            print('Login button not found.')
-
+        email_selector = "xpath=//input[@type='email']"
+        email_elem = page.wait_for_selector(email_selector)
+        email_elem.fill("dummyemail@example.com")
+        print("Email entered")
     except Exception as e:
-        print(f'An exception occurred: {str(e)}')
+        print(f"Error locating or filling in the email input box: {e}")
+        return
 
+    # Locate the submit button and click on it
+    try:
+        submit_selector = "xpath=//button[@type='submit']"
+        submit_elem = page.wait_for_selector(submit_selector)
+        submit_elem.click()
+        print("Submit button clicked")
+    except Exception as e:
+        print(f"Error locating or clicking the submit button: {e}")
+        return
+
+    # Attempt to navigate after clicking the submit button
+    try:
+        page.wait_for_load_state("networkidle")
+        print("Navigation after email submission was successful")
+    except Exception as e:
+        print(f"Error during navigation after email submission: {e}")
+        return
+
+    # Close browser
+    print("Workflow complete. Closing the browser.")
     browser.close()
+
+with sync_playwright() as playwright:
+    run(playwright)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import json
 from dotenv import load_dotenv
-from openai import OpenAI
+from openai import OpenAI, BadRequestError
 from run_code import exec_python_code
 load_dotenv()
 
@@ -17,10 +17,11 @@ Since you will not be aware of the elements and css selectors on the website, yo
 To create the script you need to decompose the workflow in multiple steps.
 Utilize XPath to find a list of relevant elements and iterate through the list to find the correct element.
 Implement 'try and except' blocks where applicable to catch errors and explore different techniques.
-Include sufficient print statements to validate if something worked instead of assuming things
+Include sufficient print statements to validate if something worked instead of assuming things.
+Add sufficient wait times for the pages to load.
 You are given a tool 'exec_python_code'. Use this tool to execute Python code and get the output of the executed code.
 Assume playwright is already installed.
-Ensure the browser is in headless=False mode.
+Ensure the browser is in headless=False mode. 
 
 Approach the task step by step and always follow the sequence of thought, action, and observation flow.
  1. Thought: Think of an action plan and share it with user.
@@ -40,33 +41,81 @@ messages.append({
 
 while True:
     print("Calling GPT API")
-    response = client.chat.completions.create(
-        messages=messages,
-        model="gpt-4-1106-preview",
-        tools=[
-            {
-                "type": "function",
-                "function": {
-                    "description": "execute Python code and get the output of the executed code.",
-                    "name": "exec_python_code",
-                    "parameters": {
-                        "type": "object",
-                        "properties": {
-                            "code_block": {
-                                "type": "string",
-                                "description": "Python code block to execute",
+    try:
+        response = client.chat.completions.create(
+            messages=messages,
+            model="gpt-4-1106-preview",
+            tools=[
+                {
+                    "type": "function",
+                    "function": {
+                        "description": "execute Python code and get the output of the executed code.",
+                        "name": "exec_python_code",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                                "code_block": {
+                                    "type": "string",
+                                    "description": "Python code block to execute",
+                                },
                             },
+                            "required": ["code_block"],
                         },
-                        "required": ["code_block"],
-                    },
+                    }
                 }
-            }
-        ]
-    )
+            ]
+        )
+    except BadRequestError as e:
+        print(messages) # Sometimes we get a bad error message when the previous message by assistant contains both message and tool_call.
+    finally:
+        response = client.chat.completions.create(
+            messages=messages,
+            model="gpt-4-1106-preview",
+            tools=[
+                {
+                    "type": "function",
+                    "function": {
+                        "description": "execute Python code and get the output of the executed code.",
+                        "name": "exec_python_code",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                                "code_block": {
+                                    "type": "string",
+                                    "description": "Python code block to execute",
+                                },
+                            },
+                            "required": ["code_block"],
+                        },
+                    }
+                }
+            ]
+        )
 
     response_message = response.choices[0].message
     messages.append(response_message)
     gpt_response = response_message.content
+    if response_message.tool_calls is not None:
+
+        for tool_call in response_message.tool_calls:
+            function_name = tool_call.function.name
+            function_args = json.loads(tool_call.function.arguments)
+            print("Execute following code block:")
+            print(function_args.get("code_block"))
+            print("Paste the output:")
+            exec_output, exec_error = exec_python_code(function_args.get("code_block"))
+            if exec_error is not None:
+                exec_output += exec_error
+            print(f"Output: {exec_output}")
+            messages.append(
+                {
+                    "tool_call_id": tool_call.id,
+                    "role": "tool",
+                    "name": function_name,
+                    "content": exec_output,
+                }
+            )
+
     if gpt_response is not None:
         print(gpt_response)
         if gpt_response == "TERMINATED":
@@ -76,24 +125,7 @@ while True:
             "role": "user",
             "content": user_prompt,
         })
-    if response_message.tool_calls is None:
-        continue
-    for tool_call in response_message.tool_calls:
-        function_name = tool_call.function.name
-        function_args = json.loads(tool_call.function.arguments)
-        print("Execute following code block:")
-        print(function_args.get("code_block"))
-        print("Paste the output:")
-        exec_output, exec_error = exec_python_code(function_args.get("code_block"))
-        if exec_error is not None:
-            exec_output += exec_error
-        print(f"Output: {exec_output}")
-        messages.append(
-            {
-                "tool_call_id": tool_call.id,
-                "role": "tool",
-                "name": function_name,
-                "content": exec_output,
-            }
-        )
+
+
+#Todo Check the token usage.
 
